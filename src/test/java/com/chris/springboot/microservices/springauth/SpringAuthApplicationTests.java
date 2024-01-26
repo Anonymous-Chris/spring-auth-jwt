c684a44b062bf64243833cc5ed6574baf658c12a
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2024-01-26 12:01:50 -0600

    learn spring security with jwt initial

---
+++ src/test/java/com/chris/springboot/microservices/springauth/SpringAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.springboot.microservices.springauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
